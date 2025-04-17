03edeb27997acd3dbffaaaf0a6d4dd498d394f05
Bryce285 <bryceclark237@gmail.com>
2025-04-17 13:38:04 -0700

    Correct holding pattern behavior for enemies

--- Scripts/enemy_ship_hold.gd
+++ Scripts/enemy_ship_hold.gd
@@ -3,31 +3,39 @@ class_name EnemyShipHold
 
 @export var enemy: CharacterBody2D
 @export var speed := 300.0
+@export var holding_pattern_size := 500.0 # increase size of holding pattern later
 
-var move_direction : Vector2
+var holding_pattern_center : Vector2
+var holding_pattern_corners = []
+var current_move_target_index = 0
+
 var move_time : float
 var objective : Area2D
 
-func randomize_wander():
-	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	move_time = randf_range(1, 3)
+func generate_holding_pattern():
+	holding_pattern_center = objective.global_position
+	var half = holding_pattern_size / 2
+	holding_pattern_corners = [
+		holding_pattern_center + Vector2(-half, -half),
+		holding_pattern_center + Vector2(half, -half),
+		holding_pattern_center + Vector2(half, half),
+		holding_pattern_center + Vector2(-half, half)
+	]
 
 func enter():
 	objective = get_tree().get_first_node_in_group("objective")
-	randomize_wander()
-
-func update(delta: float):
-	if move_time > 0:
-		move_time -= delta
-	else:
-		randomize_wander()
+	generate_holding_pattern()
 
 func physics_update(delta: float):
+	var move_target = holding_pattern_corners[current_move_target_index]
+	var direction = move_target - enemy.global_position
+	
 	if enemy:
-		enemy.velocity = move_direction * speed
+		enemy.velocity = direction.normalized() * speed
 	
-	var direction = objective.global_position - enemy.global_position
-	
+	if direction.length() < 25:
+		current_move_target_index = (current_move_target_index + 1) % holding_pattern_corners.size()
+		return
 	# increase the distance to emit when implementing better movement
-	if direction.length > 1000:
+	elif direction.length() > 1000:
 		Transitioned.emit(self, "EnemyShipMove")
